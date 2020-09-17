cb39d30408c93289a66452dd29664b71359dd172
mabonza <lmukowani@gmail.com>
2020-09-17 22:32:08 +0200

    DatingApp

--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -8,5 +8,6 @@ namespace DatingApp.API.Data
         public DataContext(DbContextOptions<DataContext> options) : base (options) {}
 
         public DbSet<value> Values { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }
